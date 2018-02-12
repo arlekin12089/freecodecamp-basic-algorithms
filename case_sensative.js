4a776832d7e4597db6de3511cc0739918d591693
Arlekin1208 <arlekin12089@gmail.com>
2018-02-12 19:42:50 +0100

    title case sensative

---
+++ case_sensative.js
@@ -0,0 +1,11 @@
+
+function titleCase(str) {
+  str =str.split(" ");
+  for(var i = 0; i<str.length; i++){
+     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1).toLowerCase();
+  }
+
+  return str.join(" ");
+}
+
+titleCase("I'm a little tea pot");
