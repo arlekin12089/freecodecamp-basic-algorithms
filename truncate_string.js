c18c87bdd6f712e339fcd7ec8ad0fed5cbda0468
Alyona <arlekin12089@gmail.com>
2020-03-06 23:32:12 +0100

    Update truncate_string.js

--- truncate_string.js
+++ truncate_string.js
@@ -7,3 +7,17 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+
+
+function truncateString(str, num) {
+  //return str.split(" ")[0] > num ? str.slice(0,num);
+  let a = str.split(" ").length;
+  if(a >= num){
+      return str.slice(0,num) + "...";
+  }else {
+    return str;
+  }
+
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
