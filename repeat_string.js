7e4e97e55e94cad90b40a942039c82e1caf540e3
Alyona <arlekin12089@gmail.com>
2020-03-06 23:08:02 +0100

    Update repeat_string.js

--- repeat_string.js
+++ repeat_string.js
@@ -1,13 +1,12 @@
 function repeatStringNumTimes(str, num) {
-  // repeat after me
-  if(num <= 0){
-    return "";
+  if(num < 0) {
+    return '';
   }
-  var final_string = [];
-    for(var i = 0; i<num; i++){
-   final_string.push(str);
-  }
-  return final_string.join("");
+  let newStr = "";
+  for(let i = 0; i < num; i++){
+    newStr += str;    
+    }
+    return newStr;
 }
 
 repeatStringNumTimes("abc", 3);
