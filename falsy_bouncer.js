c6a434d999a2ceb86409a94c57115557dcf48455
Alyona <arlekin12089@gmail.com>
2020-03-09 21:00:29 +0100

    Update falsy_bouncer.js

--- falsy_bouncer.js
+++ falsy_bouncer.js
@@ -1,3 +1,7 @@
+/*Remove all falsy values from an array.
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+Hint: Try converting each value to a Boolean.*/
+
 
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
@@ -5,3 +9,21 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+
+//OR
+
+
+function bouncer(arr) {
+  let arrNew = [];
+    arr.map((a)=>{
+    if(Boolean(a) === false){
+      delete arr[a];
+      }else{
+       arrNew.push(a);
+      }
+    })   
+   return arrNew;
+}
+
+bouncer([false, null, 0, NaN, undefined, ""]);
